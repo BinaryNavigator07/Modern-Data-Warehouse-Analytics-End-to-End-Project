2b14fdd31ac6b80303749022f33c688222a08599
BinaryNavigator07 <umaiesajid@gmail.com>
2025-05-05 06:13:17 +0500

    Update SQL script to use new dataset paths for loading bronze layer tables

--- scripts/bronze/load_bronze_client.sql
+++ scripts/bronze/load_bronze_client.sql
@@ -12,15 +12,15 @@
 
 TRUNCATE TABLE bronze.crm_cust_info;
 \echo '→ Loading crm_cust_info...'
-\copy bronze.crm_cust_info FROM 'data/bronze/crm_cust_info.csv' DELIMITER ',' CSV HEADER
+\copy bronze.crm_cust_info FROM 'datasets/source_crm/cust_info.csv' DELIMITER ',' CSV HEADER
 
 TRUNCATE TABLE bronze.crm_prd_info;
 \echo '→ Loading crm_prd_info...'
-\copy bronze.crm_prd_info FROM 'data/bronze/crm_prd_info.csv' DELIMITER ',' CSV HEADER
+\copy bronze.crm_prd_info FROM 'datasets/source_crm/prd_info.csv' DELIMITER ',' CSV HEADER
 
 TRUNCATE TABLE bronze.crm_sales_details;
 \echo '→ Loading crm_sales_details...'
-\copy bronze.crm_sales_details FROM 'data/bronze/crm_sales_details.csv' DELIMITER ',' CSV HEADER
+\copy bronze.crm_sales_details FROM 'datasets/source_crm/sales_details.csv' DELIMITER ',' CSV HEADER
 
 -- ERP TABLES
 \echo '---------------------------------------------------'
@@ -29,15 +29,15 @@ TRUNCATE TABLE bronze.crm_sales_details;
 
 TRUNCATE TABLE bronze.erp_cust_az12;
 \echo '→ Loading erp_cust_az12...'
-\copy bronze.erp_cust_az12 FROM 'data/bronze/erp_cust_az12.csv' DELIMITER ',' CSV HEADER
+\copy bronze.erp_cust_az12 FROM 'datasets/source_erp/CUST_AZ12.csv' DELIMITER ',' CSV HEADER
 
 TRUNCATE TABLE bronze.erp_loc_a101;
 \echo '→ Loading erp_loc_a101...'
-\copy bronze.erp_loc_a101 FROM 'data/bronze/erp_loc_a101.csv' DELIMITER ',' CSV HEADER
+\copy bronze.erp_loc_a101 FROM 'datasets/source_erp/LOC_A101.csv' DELIMITER ',' CSV HEADER
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 \echo '→ Loading erp_px_cat_g1v2...'
-\copy bronze.erp_px_cat_g1v2 FROM 'data/bronze/erp_px_cat_g1v2.csv' DELIMITER ',' CSV HEADER
+\copy bronze.erp_px_cat_g1v2 FROM 'datasets/source_erp/PX_CAT_G1V2.csv' DELIMITER ',' CSV HEADER
 
 -- Done
 \echo '---------------------------------------------------'
