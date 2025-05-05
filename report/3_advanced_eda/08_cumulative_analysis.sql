bee6cd2e72b60b4b9ba657cb1ba0fb221c4b740c
BinaryNavigator07 <umaiesajid@gmail.com>
2025-05-05 06:54:17 +0500

    Refactor cumulative_analysis.sql to calculate total sales per year instead of per month; update variable names for clarity and improve overall readability.

--- report/3_advanced_eda/08_cumulative_analysis.sql
+++ report/3_advanced_eda/08_cumulative_analysis.sql
@@ -12,41 +12,35 @@ SQL Functions Used:
 ===============================================================================
 */
 
--- 1. Calculate the total sales per month and the running total of sales over time 
+-- 1. Calculate the total sales per year and the running total of sales over time 
 SELECT
-	order_date,
-	total_sales,
-	SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
-	ROUND(AVG(avg_price) OVER (ORDER BY order_date), 2) AS moving_average_price
-FROM
-(
+    order_year,
+    total_sales,
+    SUM(total_sales) OVER (ORDER BY order_year) AS running_total_sales,
+    ROUND(AVG(avg_price) OVER (ORDER BY order_year), 2) AS moving_average_price
+FROM (
     SELECT 
-        EXTRACT( YEAR FROM (order_date)) AS order_date,
+        EXTRACT(YEAR FROM order_date) AS order_year,
         SUM(sales_amount) AS total_sales,
         ROUND(AVG(price), 2) AS avg_price
     FROM gold.fact_sales
     WHERE order_date IS NOT NULL
-    GROUP BY  EXTRACT( YEAR FROM (order_date))
-)
-
+    GROUP BY EXTRACT(YEAR FROM order_date)
+) t;
 
 -- 2. Calculate the total sales per month & the running total of sales over time 
 SELECT
-    months,
+    month,
     total_sales,
-    SUM(total_sales) OVER (ORDER BY months) AS running_total_sums,
-    ROUND(AVG(avg_total) OVER (ORDER BY months), 2) AS moving_average_price
+    SUM(total_sales) OVER (ORDER BY month) AS running_total_sums,
+    ROUND(AVG(avg_total) OVER (ORDER BY month), 2) AS moving_average_price
 FROM (
     SELECT 
-        EXTRACT(MONTH FROM order_date) AS months,
+        EXTRACT(MONTH FROM order_date) AS month,
         SUM(sales_amount) AS total_sales,
         AVG(price) AS avg_total
-    FROM 
-        gold.fact_sales
-    WHERE
-        order_date IS NOT NULL
-    GROUP BY 
-        EXTRACT(MONTH FROM order_date)
+    FROM gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY EXTRACT(MONTH FROM order_date)
 ) subquery
-ORDER BY
-    months;
+ORDER BY month;
